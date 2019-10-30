c5b313f2ae00c8108a791360a8e72f2f550de1fd
Axel Camara <axel.camara@hotmail.fr>
2019-10-30 14:52:57 +0100

    add tp2 and tp3

--- src/main/scala/paristech/Preprocessor.scala
+++ src/main/scala/paristech/Preprocessor.scala
@@ -2,6 +2,7 @@ package paristech
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 object Preprocessor {
 
@@ -27,6 +28,7 @@ object Preprocessor {
       .config(conf)
       .appName("TP Spark : Preprocessor")
       .getOrCreate()
+    import spark.implicits._
 
     /*******************************************************************************
       *
@@ -40,8 +42,76 @@ object Preprocessor {
       *
       ********************************************************************************/
 
-    println("\n")
-    println("Hello World ! from Preprocessor")
-    println("\n")
+    var df = spark.read
+      .option("header", value = true)// utilise la première ligne du (des) fichier(s) comme header
+      .option("inferSchema", "true") // pour inférer le type de chaque colonne (Int, String, etc.)
+      .csv("data/train.csv")
+      .withColumn("desc", regexp_replace($"desc", "\"", ""))
+      // We cast values to Int, we loose information about people who write instead description of put a numbers
+      .withColumn("goal", $"goal".cast("Int"))
+      .withColumn("created_at", to_date(from_unixtime($"created_at")))
+      .withColumn("state_changed_at", to_date(from_unixtime($"state_changed_at")))
+      .withColumn("launched_at", to_date(from_unixtime($"launched_at")))
+      .withColumn("deadline", to_date(from_unixtime($"deadline")))
+      .withColumn("number_keywords", size(split(lower($"keywords"), "-")))
+      .withColumn("name", lower($"name"))
+      .withColumn("final_status", $"final_status".cast("Int"))
+      //Keep only 1 and 0 values because other are not comprehensible
+      .filter("final_status == 1 or final_status == 0")
+
+    /*******************************************************************************
+      *
+      * We Remove :
+      * - the disable_communication column. This column is very largely false, there are only 322 true (negligible),
+      *   the rest is unidentified:
+      * - The backer_count column because this feature will not be available when the module will use in production
+      *   (futur information)
+      * - state_changed_at, futur information
+      *
+      ********************************************************************************/
+
+    df = df.drop("disable_communication", "state_changed_at", "backers_count")
+
+    /*******************************************************************************
+      *
+      * Clean country and currency columns
+      *
+      ********************************************************************************/
+
+    df = df.withColumn("country2", when($"country" === "False", $"currency").otherwise($"country"))
+      .withColumn("currency2", when($"country".isNotNull && length($"currency") =!= 3, null).otherwise($"currency"))
+      .drop("country", "currency")
+
+    /*******************************************************************************
+      *
+      * Get the campaign duration
+      *
+      ********************************************************************************/
+
+    // Duration of the campaign
+    df = df.withColumn("days_campaign", datediff($"deadline", $"launched_at"))
+      .withColumn("hours_prepa", abs(datediff($"created_at", $"launched_at")*24))
+      .drop("deadline", "launched_at", "created_at")
+
+    /*******************************************************************************
+      *
+      * Concat all text columns (desc, name, keywords) for TF-IDF
+      *
+      ********************************************************************************/
+
+    df = df.withColumn("text", concat($"name", lit(" "), $"desc", lit(" "), $"keywords"))
+
+    /*******************************************************************************
+      *
+      * Replace nan values by "unknown" and save the clean dataset
+      *
+      ********************************************************************************/
+
+    df = df.na.fill(-1, Seq("days_campaign", "hours_prepa", "goal"))
+      .na.fill("unknown", Seq("currency2"))
+      .na.fill("unknown", Seq("text"))
+      .withColumn("country2", regexp_replace($"country2", "false", "unknown"))
+
+    df.write.parquet("data/kickstarter_data_clean")
   }
 }
--- src/main/scala/paristech/Trainer.scala
+++ src/main/scala/paristech/Trainer.scala
@@ -2,6 +2,11 @@ package paristech
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.feature._
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.tuning.{TrainValidationSplit, ParamGridBuilder}
 
 
 object Trainer {
@@ -27,20 +32,145 @@ object Trainer {
       .getOrCreate()
 
 
-    /*******************************************************************************
+    /** *****************************************************************************
       *
-      *       TP 3
+      * TP 3
       *
       *       - lire le fichier sauvegarder précédemment
       *       - construire les Stages du pipeline, puis les assembler
       *       - trouver les meilleurs hyperparamètres pour l'entraînement du pipeline avec une grid-search
       *       - Sauvegarder le pipeline entraîné
       *
-      *       if problems with unimported modules => sbt plugins update
+      * if problems with unimported modules => sbt plugins update
+      *
+      * *******************************************************************************/
+
+    /** *****************************************************************************
+      *
+      * Create all stage for the pipeline model :
+      *   - tokenizer
+      *   - remove stop words
+      *   - tf-idf
+      *   - one-hot-encoding for categorical values (currency and country)
+      *   - create one columns features by
+      *
+      * *******************************************************************************/
+    print("Create all stages for the pipeline... ")
+    val tokenizer = new RegexTokenizer()
+      .setPattern("\\W+")
+      .setGaps(true)
+      .setInputCol("text")
+      .setOutputCol("tokens")
+
+    val stop_words_remover = new StopWordsRemover().setInputCol("tokens")
+      .setOutputCol("whithout_stops_words")
+
+    val count_vectorizer = new CountVectorizer()
+      .setInputCol("whithout_stops_words").setOutputCol("raw_features")
+
+    val idf_model = new IDF().setInputCol("raw_features").setOutputCol("tfidf")
+
+    val currency_indexer = new StringIndexer()
+      .setInputCol("currency2")
+      .setOutputCol("currency_encoded")
+      .setHandleInvalid("keep")
+
+    val country_indexer = new StringIndexer()
+      .setInputCol("country2")
+      .setOutputCol("country2_encoded")
+      .setHandleInvalid("keep")
+
+    val encoder_model = new OneHotEncoderEstimator()
+      .setInputCols(Array("country2_encoded", "currency_encoded"))
+      .setOutputCols(Array("country_onehot", "currency_onehot"))
+
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("tfidf", "days_campaign", "hours_prepa", "goal", "country_onehot", "currency_onehot"))
+      .setOutputCol("features")
+
+    val lr = new LogisticRegression()
+      .setElasticNetParam(0.0)
+      .setFitIntercept(true)
+      .setFeaturesCol("features")
+      .setLabelCol("final_status")
+      .setStandardization(true)
+      .setPredictionCol("predictions")
+      .setRawPredictionCol("raw_predictions")
+      .setThresholds(Array(0.7, 0.3))
+      .setTol(1.0e-6)
+      .setMaxIter(20)
+
+    val pipeline = new Pipeline()
+      .setStages(Array(tokenizer, stop_words_remover,
+        count_vectorizer, idf_model, currency_indexer,
+        country_indexer, encoder_model, assembler, lr))
+
+    /** *****************************************************************************
+      *
+      * Split the dataset into training and test data
+      *
+      * *******************************************************************************/
+    print(" Pipeline created!\nLoad and Split the data into training and testing datasets... ")
+    val df = spark.read
+      .option("header", value = true)
+      .option("inferSchema", "true")
+      .parquet("data/kickstarter_data_clean")
+
+    val Array(training, test) = df.randomSplit(Array(0.9, 0.1), seed = 42)
+
+    /** *****************************************************************************
+      *
+      * Test and save the model
+      *
+      * *******************************************************************************/
+    println("Train and test datasets created!\nTrain the pipeline... ")
+    val pipeline_model = pipeline.fit(training)
+    var dfWithSimplePredictions = pipeline_model.transform(test)
+    var metrics = new MulticlassClassificationEvaluator()
+      .setMetricName("f1")
+      .setLabelCol("final_status")
+      .setPredictionCol("predictions").evaluate(dfWithSimplePredictions)
+    dfWithSimplePredictions.groupBy("final_status", "predictions").count().show()
+    println("F1-score: " + metrics)
+    pipeline_model.write.overwrite().save("model/lr_model")
+
+    /** *****************************************************************************
+      *
+      * Train the model with a "Grid search" and "train validation split" method
+      *
+      * *******************************************************************************/
+    println("Train a new model with a 'Grid search' and the 'train validation split method'...")
+    val multiclass_evaluator = new MulticlassClassificationEvaluator()
+      .setMetricName("f1")
+      .setLabelCol("final_status")
+      .setPredictionCol("predictions")
+
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(count_vectorizer.minDF, 55.0.to(95.0).by(20.0).toArray)
+      .addGrid(lr.regParam, Array(10e-8, 10e-6, 10e-4, 10e-2))
+      .build()
+
+    val trainValidationSplit = new TrainValidationSplit()
+      .setEstimator(pipeline)
+      .setEvaluator(multiclass_evaluator)
+      .setEstimatorParamMaps(paramGrid)
+      .setTrainRatio(0.7)
+      .setParallelism(2)
+
+    val model = trainValidationSplit.fit(training)
+
+    /*******************************************************************************
+      *
+      * Evaluate and save the model
       *
       ********************************************************************************/
-
-    println("hello world ! from Trainer")
-
+    dfWithSimplePredictions = model.bestModel.transform(test)
+    metrics = new MulticlassClassificationEvaluator()
+      .setMetricName("f1")
+      .setLabelCol("final_status")
+      .setPredictionCol("predictions").evaluate(dfWithSimplePredictions)
+    dfWithSimplePredictions.groupBy("final_status", "predictions").count().show()
+    println("F1-score: " + metrics)
+    pipeline_model.write.overwrite().save("model/lr_model_grid_search")
   }
 }
